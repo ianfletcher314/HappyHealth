72f6c5106e3cb3a0223028c8f62323524f2c34e1
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-26 13:25:11 -0500

    deployment through atlas

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -16,9 +15,20 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
   
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/happyhealth",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
   
 // Start the API server
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+// mongodb+srv://Health-admin:StayHealthy123@cluster0.iidhp.mongodb.net/happyhealth?retryWrites=true&w=majority
