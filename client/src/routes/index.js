717ef0f700df775200a57b0b63b9e987dd654a93
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-28 21:43:02 -0500

    added /test route

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,10 +2,17 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+router.route("/test")
+  .get((req,res)=>{
+    console.log("routes is working")
+    res.status(222).json({message:"thank the lord"})
+  })
 router.use("/api", apiRoutes);
 
 router.use(function(req, res) {
+  // console.log("routes is working")
     res.sendFile(path.join(__dirname, "../../public/index.html"));
+  
   });
 
 
