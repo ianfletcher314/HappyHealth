45b9cd305b069b2921d81f118c62f695cc712ca0
Kyle Rather <krather7@gmail.com>
2021-06-30 21:27:36 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./App.css";
 import Drawer from "./components/Drawer"
 import SignInSide from "./components/SignInSide"
-import SearchBar from "./components/SearchBar"
 
 const App = () => {
   return (
     <>
       <Drawer />
       <SignInSide />
-      <SearchBar />
     </>
   );
 };
+
+
+
+
+
+
 
 export default App;
