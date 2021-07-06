1fd7a4bda1248220c250d8fdce164c9762c98d41
Kyle Rather <krather7@gmail.com>
2021-07-06 16:28:17 -0500

    home page almost finished

--- client/src/components/Drawer/index.jsx
+++ client/src/components/Drawer/index.jsx
@@ -6,18 +6,25 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { withRouter } from "react-router-dom";
+import HomeIcon from '@material-ui/icons/Home';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import StoreIcon from '@material-ui/icons/Store';
+import EditIcon from '@material-ui/icons/Edit';
+import PersonIcon from '@material-ui/icons/Person';
+import './style.css'
 
-const drawerWidth = 240;
+
+const drawerWidth = 200;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -61,27 +68,54 @@ function ResponsiveDrawer(props) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+  const {history}=props;
+  const itemsList = [
+    {
+      text: "Home",
+      icon:<HomeIcon />,
+      onClick: () => history.push('/')
+    },
+    {
+      text: "Log In",
+      icon:<LockOpenIcon />,
+      onClick: () => history.push('/login')
+    },
+    {
+      text: "Profile",
+      icon:<PersonIcon />,
+      onClick: () => history.push('/profile')
+    },
+    {
+      text: "Shopping List",
+      icon:<StoreIcon />,
+      onClick: () => history.push('/shopping')
+    },
+    {
+      text: "Ingredients",
+      icon:<FormatListNumberedIcon />,
+      onClick: () => history.push('/ingredients')
+    },
+    {
+      text: "Sign Up",
+      icon:<EditIcon />,
+      onClick: () => history.push('/signup')
+    },
+  ]
 
   const drawer = (
     <div>
       <div className={classes.toolbar} />
       <Divider />
       <List>
-        {['Log In', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+        {itemsList.map((item, index) => {
+          const {text,icon,onClick}=item;
+          return(
+          <ListItem button key={text} onClick={onClick}>
+            {icon && <ListItemIcon>{icon}</ListItemIcon>}
             <ListItemText primary={text} />
           </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+          );
+          })}
       </List>
     </div>
   );
@@ -89,7 +123,9 @@ function ResponsiveDrawer(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
+    
     <div className={classes.root}>
+      
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
@@ -145,6 +181,8 @@ function ResponsiveDrawer(props) {
   );
 }
 
+
+
 ResponsiveDrawer.propTypes = {
   /**
    * Injected by the documentation to work in an iframe.
@@ -153,4 +191,4 @@ ResponsiveDrawer.propTypes = {
   window: PropTypes.func,
 };
 
-export default ResponsiveDrawer;
+export default withRouter(ResponsiveDrawer);
